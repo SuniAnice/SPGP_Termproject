d9bcf50c3cbc4b091c695f8cc6618850b8cd61ae
Sung Ki Hong <hgbb3210@naver.com>
2021-03-26 11:59:59 +0900

    padding, margin 적용

--- Animation/app/src/main/java/kr/ac/kpu/game/s2016182019/animation/MyView.java
+++ Animation/app/src/main/java/kr/ac/kpu/game/s2016182019/animation/MyView.java
@@ -4,24 +4,28 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MyView extends View {
     private static final String TAG = MyView.class.getSimpleName();
     private Paint paint = new Paint();
     private Rect rect = new Rect();
 
-    public MyView(Context context) {
-        super(context);
+    public MyView(Context context, AttributeSet set) {
+        super(context, set);
         paint.setColor(0xff0055ff);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        int w = getWidth();
-        int h = getHeight();
-        rect.set(0, 0, w, h);
+        int l = 0 + getLeftPaddingOffset();
+        int t = 0 + getTopPaddingOffset();
+        int w = getWidth() - getRightPaddingOffset();
+        int h = getHeight() - getBottomPaddingOffset();
+        rect.set(l, t, w, h);
         Log.d(TAG, "drawing " + rect);
         canvas.drawRect(rect, paint);
     }
