ac4b84d546b5c0bff410811bc0d55550418133d9
Sung Ki Hong <hgbb3210@naver.com>
2021-04-05 11:01:56 +0900

    MainGame 추가

---
+++ SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182019/samplegame/game/MainGame.java
@@ -0,0 +1,61 @@
+package kr.ac.kpu.game.s2016182019.samplegame.game;
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import kr.ac.kpu.game.s2016182019.samplegame.framework.GameObject;
+
+public class MainGame {
+    public static final int BALL_COUNT = 10;
+
+    // singleton
+    static MainGame instance;
+    public static MainGame get() {
+        if (instance == null) {
+            instance = new MainGame();
+        }
+        return instance;
+    }
+
+    ArrayList<GameObject> objects = new ArrayList<>();
+    Player player;
+    public float frameTime;
+
+    public void initResources() {
+        player = new Player(100, 100, 0, 0);
+        Random rand = new Random();
+        for (int i = 0; i < BALL_COUNT; i++) {
+            float x = rand.nextInt(1000);
+            float y = rand.nextInt(1000);
+            float dx = rand.nextFloat() * 1000 - 500;
+            float dy = rand.nextFloat() * 1000 - 500;
+            Ball b = new Ball(x, y, dx, dy);
+            objects.add(b);
+        }
+        objects.add(player);
+    }
+
+    public void update() {
+        for (GameObject o : objects) {
+            o.update();
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        for (GameObject o : objects) {
+            o.draw(canvas);
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
+            player.moveTo(event.getX(), event.getY());
+            return  true;
+        }
+        return false;
+    }
+}
--- SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182019/samplegame/ui/view/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182019/samplegame/ui/view/GameView.java
@@ -9,29 +9,21 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-import kr.ac.kpu.game.s2016182019.samplegame.game.Ball;
-import kr.ac.kpu.game.s2016182019.samplegame.framework.GameObject;
-import kr.ac.kpu.game.s2016182019.samplegame.game.Player;
+import kr.ac.kpu.game.s2016182019.samplegame.game.MainGame;
 
 public class GameView extends View {
     private static final String TAG = GameView.class.getSimpleName();
-    public static final int BALL_COUNT = 10;
 
     private long lastFrame;
-    public static float frameTime;
     public static GameView view;
 
-//    ArrayList<Ball> balls = new ArrayList<>();
-    ArrayList<GameObject> objects = new ArrayList<>();
-    Player player;
+
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         GameView.view = this;
-        initResources();
+        MainGame game = MainGame.get();
+        game.initResources();
         startUpdating();
     }
 
@@ -41,9 +33,8 @@ public class GameView extends View {
 
     private void doGameFrame() {
 //        update();
-        for (GameObject o : objects) {
-            o.update();
-        }
+        MainGame game = MainGame.get();
+        game.update();
 
 //        draw();
         invalidate();
@@ -54,49 +45,29 @@ public class GameView extends View {
                 if (lastFrame == 0){
                     lastFrame = time;
                 }
-                frameTime = (float) (time - lastFrame) / 1_000_000_000;
+                game.frameTime = (float) (time - lastFrame) / 1_000_000_000;
                 doGameFrame();
                 lastFrame = time;
 
             }
         });
 
-//        postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                doGameFrame();
-//            }
-//        },15);
+
     }
 
-    private void initResources() {
-        player = new Player(100, 100, 0, 0);
-        Random rand = new Random();
-        for (int i = 0; i < BALL_COUNT; i++) {
-            float x = rand.nextInt(1000);
-            float y = rand.nextInt(1000);
-            float dx = rand.nextFloat() * 1000 - 500;
-            float dy = rand.nextFloat() * 1000 - 500;
-            Ball b = new Ball(x, y, dx, dy);
-            objects.add(b);
-        }
-        objects.add(player);
-    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
-        for (GameObject o : objects) {
-            o.draw(canvas);
-        }
+        MainGame game = MainGame.get();
+        game.draw(canvas);
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getAction();
-        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
-            player.moveTo(event.getX(), event.getY());
-            return  true;
-        }
-        return false;
+        MainGame game = MainGame.get();
+        return game.onTouchEvent(event);
+
     }
 }
