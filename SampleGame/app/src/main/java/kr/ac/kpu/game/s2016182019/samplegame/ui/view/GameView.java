3f194292ae7a1d795e131fbf359760840e38f9ee
Sung Ki Hong <hgbb3210@naver.com>
2021-04-16 11:33:07 +0900

    SoundPool을 활용한 소리 재생

--- SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182019/samplegame/ui/view/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/game/s2016182019/samplegame/ui/view/GameView.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import kr.ac.kpu.game.s2016182019.samplegame.framework.Sound;
 import kr.ac.kpu.game.s2016182019.samplegame.game.MainGame;
 
 public class GameView extends View {
@@ -23,6 +24,7 @@ public class GameView extends View {
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         GameView.view = this;
+        Sound.init(context);
         //startUpdating();
     }
 
